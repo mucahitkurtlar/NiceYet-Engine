1b73ccbc7746113c7465bfeded40388f41a04042
Mucahit KURTLAR <mucahit@tutanota.com>
2020-12-17 20:00:40 +0300

    Add createRect function to TextureManager class

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -1,9 +1,19 @@
 #include "TextureManager.h"
 
-SDL_Texture* TextureManager::LoadTexture(const char* filename, SDL_Renderer* renderer) {
+SDL_Texture* TextureManager::loadTexture(const char* filename, SDL_Renderer* renderer) {
     SDL_Surface* tempSurface = SDL_LoadBMP(filename);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, tempSurface);
     SDL_FreeSurface(tempSurface);
 
     return texture;
+}
+
+SDL_Rect TextureManager::createRect(int h, int w, int x,int y) {
+    SDL_Rect temprect;
+    temprect.h = h;
+    temprect.w = w;
+    temprect.x = x;
+    temprect.y = y;
+
+    return temprect;
 }
--- src/TextureManager.h
+++ src/TextureManager.h
@@ -5,5 +5,6 @@
 class TextureManager
 {
 public:
-    static SDL_Texture* LoadTexture(const char* filename, SDL_Renderer* renderer);
+    static SDL_Texture* loadTexture(const char* filename, SDL_Renderer* renderer);
+    static SDL_Rect createRect(int h = 32, int w = 32, int x = 0,int y = 0);
 };
